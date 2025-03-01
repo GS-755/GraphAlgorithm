96fe5b13d21ea4a6ea9d755157f03ae88be4391e
Trí Nguyễn a.k.a @MianXinhDep <tringuyenminh2003@gmail.com>
2025-03-01 20:11:24 +0700

    Add merge source code feature for final release

---
+++ Constants/GitInfo.cs
@@ -0,0 +1,7 @@
+﻿namespace ConsoleApp1.Constants
+{
+    public class GitInfo
+    {
+        public static readonly string GIT_BRANCH = "DEVELOP";
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,31 @@
-﻿using ConsoleApp1.BaiTap;
+﻿using System;
+using ConsoleApp1.BaiTap;
 
 namespace ConsoleApp1
 {
     public class Program
     {
+        static void ShowHelp()
+        {
+            Console.WriteLine("GraphAlgorithm @GS-755");
+            Console.WriteLine($"Branch: {GitInfo.GIT_BRANCH}");
+            Console.WriteLine("\nTo run project: No need to parse args!");
+            Console.WriteLine("Note: To build release: use --merge-source argument"); 
+        }
         public static void Main(string[] args)
         {
+            if(args.Length > 0)
+            {
+                if(args[0] == "--merge-source")
+                {
+                    MergeReleaseSource.Execute();
+                }
+                else
+                {
+                    ShowHelp();
+                }
+                Environment.Exit(0);
+            }
             Buoi2.Run();
             Buoi3.Run();
         }
